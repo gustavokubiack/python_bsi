3d77bbe8556db7536589e732d410d7d8339740be
gustavokubiack <guukubiack@gmail.com>
2023-11-20 20:21:50 -0300

    Exercícios de tratamento de exceções.

--- erros_excecoes/exercicios.py
+++ erros_excecoes/exercicios.py
@@ -1,7 +1,4 @@
-"""" Escreva uma função que recebe dois números como argumentos e retorna a
-divisão do primeiro pelo segundo. Use um bloco try/except para tratar o caso em
-que o segundo número é zero e lance uma exceção personalizada com a
-mensagem "Divisão por zero não permitida"."""
+# Exercício 1
 
 def divide_num(num1, num2):
     try:
@@ -11,22 +8,22 @@ def divide_num(num1, num2):
 
 print(divide_num(2, 1))
 
+# Exercício 2
 
-"""Escreva um programa que solicita ao usuário uma data no formato "dd/mm/aaaa"
-e verifica se ela é válida. Use um bloco `try/except` para tratar o caso em que o
-usuário digita uma data inválida e lance uma exceção personalizada com a
-mensagem “Data inválida”."""
+from datetime import datetime
 
-def input_date(date):
+def input_date():
     try:
-        pass
-    except:
-        pass
+        data = input("Digite uma data no formato dd/mm/aaaa: ")
+        formatt = '%d/%m/%Y'
+        formatted_data = datetime.strptime(data, formatt)
+        return (f"Data válida: {formatted_data}")
+    except ValueError:
+        return "Data inválida"
 
-"""Escreva uma função que recebe uma lista de números como argumento e retorna a
-soma dos elementos da lista. Use um bloco `try/except` para tratar o caso em que
-a lista contém algum elemento que não é um número e lance uma exceção
-personalizada com a mensagem “Lista inválida”."""
+print(input_date())
+
+# Exercício 3
 
 def sum_list(lista):
     try:
@@ -39,11 +36,7 @@ nums.append("7")
 print(sum_list(nums))
 
 
-"""
-Escreva um programa que solicita ao usuário um nome de arquivo e tenta abri-lo
-para leitura. Use um bloco `try/except` para tratar o caso em que o arquivo não
-existe ou não pode ser aberto e lance uma exceção personalizada com a
-mensagem “Arquivo inválido”. """
+# Exercício 4
 
 def input_file():
     try:
@@ -57,14 +50,12 @@ def input_file():
 print(input_file())
 
 
-"""Escreva um código que tente abrir um arquivo com o modo de escrita, porém o
-arquivo já existe. Se ocorrer uma exceção, imprima uma mensagem de erro."""
+# Exercício 5
 
-import os
 
 def check_file():
     try:
-        with open("usuarios.txt", "x") as f:
+        with open("erros_excecoes/usuarios.txt", "x") as f:
             f.write("Olá")
     except:
         return "Arquivo já existe!"
@@ -72,10 +63,7 @@ def check_file():
 print(check_file())
 
 
-"""Escreva uma função que recebe um número inteiro positivo como argumento e
-retorna o fatorial desse número. Use um bloco `try/except` para tratar o caso em
-que o argumento é negativo ou não é um inteiro e lance uma exceção
-personalizada com a mensagem “Argumento inválido”."""
+# Exercício 6
 
 import math
 
@@ -88,10 +76,7 @@ def factor_num(num):
 num = -1.5
 print(factor_num(num))
 
-"""Escreva uma função que recebe uma string como argumento e retorna o número
-de vogais contidas nessa string. Use um bloco `try/except` para tratar o caso em
-que o argumento não é uma string e lance uma exceção personalizada com a
-mensagem “Argumento inválido”."""
+# Exercício 7
 
 def count_vowels(word):
     try:
@@ -104,10 +89,7 @@ word = "aeiou"
 print(count_vowels(word))
 
 
-"""Escreva uma função que recebe uma lista de strings como argumento e retorna
-uma nova lista com estas strings ordenadas alfabeticamente. Use um bloco
-`try/except` para tratar o caso em que a lista contém algum elemento que não é
-uma string e lance uma exceção personalizada com a mensagem “Lista inválida”."""
+# Exercício 8
 
 def order_list(lista):
     try:
@@ -117,9 +99,7 @@ def order_list(lista):
 
 print(order_list(lista=["a", "x", 'b', 1]))
 
-"""Escreva um programa que solicite ao usuário um índice e, em seguida, tente
-acessar um elemento em uma lista. Trate exceções caso o índice esteja fora dos
-limites da lista."""
+# Exercício 9
 
 def check_index(lista):
     try:
@@ -129,3 +109,155 @@ def check_index(lista):
         return "Índice não está na lista!"
 
 print(check_index(lista=["1", "2", "3", "4", "5"]))
+
+# Exercício 10
+
+def convert_number_to_string():
+    try:
+        return str(input("Insira um número inteiro para converter para string: "))    
+    except:
+        return "Erro!"
+    
+print(convert_number_to_string())
+
+# Exercício 11
+
+def format_text():
+    try:
+        import os
+
+        file_path = "erros_excecoes/texto.txt"
+        file_path = os.path.abspath(file_path)
+        with open(file_path, "r+") as f:
+            content = f.read()
+            content = ' '.join(content.split())
+            f.seek(0)
+            f.truncate(0)
+            f.write(content)
+    except Exception as e:
+        return 'Erro ao formatar arquivo', e
+    
+    return 'Arquivo formatado com sucesso'
+
+print(format_text())
+
+
+# Exercício 12
+
+data = {
+    "name": "Gustavo Kubiack",
+    "age": 20,
+    "height": 1.87,
+    "is_programmer": True,
+}
+
+def access_dict(data):
+    try:
+        key = input("Insira uma chave do dicionário: ")
+        return data[key]
+    except KeyError:
+        return "Não foi possível acessar o dicionário, tente outra chave!"
+
+print(access_dict(data))
+
+# Exercício 13
+def sum():
+    try:
+        num1 = int(input("Insira o primeiro número inteiro: "))
+        num2 = int(input("Insira o segundo número inteiro: "))
+        return num1 + num2
+    except Exception:
+        return f"Erro! É necessário inserir valores inteiros de 0 a 9."
+
+print(sum())
+
+
+# Exercício 14
+
+def input_password():
+    password = input("Insira uma senha com 8 caracteres: ")
+    try:
+        if len(password) >= 8:
+            return password
+        else:
+            raise ValueError("Senha muito curta! Insira no mínimo 8 caracteres :)")
+    except ValueError as e:
+        return e
+    
+print(input_password())
+
+# Exercício 15
+
+def divide_list():
+    try:
+        nums = [_ for _ in range(1, 11)]
+        divisor = int(input("Insira um divisor para a lista: "))
+        sub_list_len = len(nums) // divisor
+        resultado = [nums[i * sub_list_len: (i + 1) * sub_list_len] for i in range(divisor)]
+        return resultado
+    except:
+        return "Não é possível dividir a lista em partes iguais!"
+    
+print(divide_list())
+
+# Exercício 16
+
+def show_number():
+    try:
+        return int(input("Insira um número inteiro: "))
+    except ValueError:
+        return "Insira somente valores inteiros."
+    except TypeError:
+        return "O tipo de entrada não pode ser convertido para um número."
+    except:
+        return "Algum erro aconteceu :("
+    
+print(show_number())
+
+# Exercício 17
+
+def add_numbers():
+    try:
+        while True:
+            nums = []
+            num = float(input("Insira um número na lista: "))
+            nums.append(num)
+    except:
+        return "Insira somente valores números!"
+            
+print(add_numbers())
+
+# Exercício 18
+
+def factorial():
+    try:
+        num = int(input("Informe um número inteiro: "))
+        try:
+            if num > 0:
+                return math.factorial(num)
+            else:
+                raise ValueError("Não é possível realizar o fatorial de valores negativos")
+        except ValueError as e:
+            return e
+    except Exception:
+        return "Erro! É necessário inserir um valor inteiro!"
+    
+print(factorial())
+
+
+# Exercício 19
+
+def higher_number():
+    try:
+        num1 = int(input("Insira o primeiro número inteiro: "))
+        num2 = int(input("Insira o segundo número inteiro: "))
+        if num1 > num2:
+            return num1
+        else:
+            return num2 
+    except ValueError:
+        return "Erro: Insira somente valores inteiros!"
+    except TypeError:
+        return "Erro de tipagem!"
+    else:
+        return "Aconteceu algo!"
